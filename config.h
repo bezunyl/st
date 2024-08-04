c8acfdc9138d13f7b41edefc439805f310e6cc10
Benedikt Zundel <benedikt.zundel@live.com>
2024-08-04 18:13:17 +0200

    change terminal font to terminus

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Perfect DOS VGA 437:pixelsize=14:antialias=true:autohint=true";
+static char *font = "terminus:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
